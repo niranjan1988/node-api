8e1f563dba1447ba780610bdf16e87572c9a9a4e
niranjan1988 <valliveduniranjan@gmail.com>
2021-04-13 11:41:48 +0530

    Edit and delete api

--- controllers/drivers_controllers.js
+++ controllers/drivers_controllers.js
@@ -10,5 +10,22 @@ module.exports={
         Driver.create(body).then(driver=>{
             res.send(driver);
         }).catch(next)
+    },
+
+    edit(req,res,next) {
+        const driverId = req.params.id;
+        const driverProps = req.body;
+
+        Driver.findByIdAndUpdate({_id:driverId}, driverProps)
+        .then(()=> Driver.findById(driverId))
+        .then(driver=>res.send(driver))
+        .catch(next);
+    },
+
+    delete(req,res, next) {
+        const driverId = req.params.id;
+        Driver.findByIdAndDelete(driverId)
+        .then(driver=>res.status(204).send(driver))
+        .catch(next);
     }
 }
--- test/controllers/drivers_controllers_test.js
+++ test/controllers/drivers_controllers_test.js
@@ -9,13 +9,43 @@ describe('Drivers Controller', (done) => {
         Driver.count().then(count => {
             request(app)
                 .post('/api/drivers')
-                .send({ email: 'test@test.com' })
+                .send({ "email": 'test@test.com' })
                 .end(() => {
                     Driver.count().then(newCount => {
                         assert(count + 1 == newCount);
                         done();
                     })
                 })
+        })
+    });
+
+    it('Put to /api/driver to edit an existing driver', (done) => {
+        const driver = new Driver({ email: 't@t.com' });
+        driver.save().then(() => {
+            request(app)
+                .put('/api/drivers/' + driver._id)
+                .send({ driving:true })
+                .end(() => {
+                    Driver.findById(driver._id).then(d => {
+                        assert(d.driving == true);
+                        done();
+                    })
+                })
+        })
+    });
+
+    it('Delete to /api/drivers/:id should delete a driver', () => {
+        const driver = new Driver({email:'test@email.com'});
+        driver.save().then(()=>{
+            request(app)
+            .delete('/api/drivers/'+driver._id)
+            .end(()=>{
+                Driver.findById(driver._id)
+                .then(driver =>{                   
+                    assert(driver === null)
+                    done();
+                })
+            })
         })
     });
 });
